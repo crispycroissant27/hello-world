74f0c0f06a071560d0061c3759d5b8eb4ca965b5
Isabelle Barker <isabellebarker@Isabelles-MBP.cgocable.net>
2020-11-30 01:03:28 -0500

    some changes

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-x= ("hello world")
-print (x)
-print ("hey!")
+a=complex(3,6)
+print (type(a))
+print (a)
+s="my name is isabelle"
+print (s[5:10])
+print ((s)+" and i love bread")
+mylist=['bread', 4, 'lights', s]
+print (mylist)
